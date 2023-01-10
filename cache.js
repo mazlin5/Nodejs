49a51436e27c9f26614112613b1f701b46dac83d
Malik Lindsay <mazlin@Maliks-MacBook-Air.local>
2023-01-09 21:35:53 -0500

    added comments to caching code

--- cache.js
+++ cache.js
@@ -1,15 +1,20 @@
+// import dependencies
 const database = require('./database');
 const express = require('express');
 
+// create express instance as app object
 const app = express();
+// set up key-value store for cashing layer
 const cache = {};
 
+// if there was no cache, hit server directly
 app.get('/nocache/index.html', (req, res) => {
     database.get('index.html', page => {
         res.send()
     });
 });
 
+// if there was cache, try cache first else, hit server 
 app.get('/withcache/index.html', (req, res) => {
     if ('index.html' in cache) {
         res.send(cache['index.html']);
@@ -22,6 +27,7 @@ app.get('/withcache/index.html', (req, res) => {
     });
 });
 
+// setup server to listen on localhost port for incoming requests
 app.listen(3001, function() {
     console.log('listening on port 3001!');
 });
